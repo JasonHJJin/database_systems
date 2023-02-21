7081f295200b35a0ec07ad38aa1f0d6b8a3f5da0
Jason Jin <jinjasonhj@gmail.com>
2023-02-21 16:08:07 -0500

    coms4111

---
+++ HW_Assignments/F22_W4111_HW_0/HW0 Test/HW0PyCharmTest.py
@@ -0,0 +1,7 @@
+# PyCharm Test
+
+# Replace the abc123 with your uni
+uni = "hj2602"
+
+print(uni)
+print("Hello World!")
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# database_systems
+# W4111-Intro-to-DB-S22
+Intro. to Databases, sections 002, V02, Spring 2022
